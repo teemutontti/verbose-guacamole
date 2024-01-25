f61cd74193476468ec4f5254211df7d4b013daec
Teemu Tontti <teemu.tontti.tt@gmail.com>
2024-01-26 01:17:16 +0200

    ../utilities.js: Add script that holds all useful functions.

---
+++ frontend/src/utilities.js
@@ -0,0 +1,9 @@
+const calculatePep = (calories, proteins) => {
+    const proteinsTimed = proteins * 4;
+    const proteinsInCalories = proteinsTimed / calories;
+    const proteinEfficiencyPercentage = proteinsInCalories * 100;
+
+    return proteinEfficiencyPercentage.toFixed(1);
+}
+
+export default calculatePep;
