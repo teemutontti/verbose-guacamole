a05e99ce5b3ee7402d36920d11dbe5802ca24bff
Teemu Tontti <teemu.tontti.tt@gmail.com>
2024-01-26 01:13:38 +0200

    ../colorTheme.js: Add file for managing color theme.

---
+++ frontend/src/colorTheme.js
@@ -0,0 +1,34 @@
+const theme = {
+    dark: {
+        primary: {
+            light: "#004080",
+            main: "#003366",
+            dark: "#001F3F",
+        },
+        secondary: {
+            light: '#333333',
+            main: '#1A1A1A',
+            dark: '#000000',
+        },
+        accent: {
+            main: "#FFA500",
+        }
+    },
+    light: {
+        primary: {
+            light: "#C0C0C0",
+            main: "#B0C4DE",
+            dark: "#ADD8E6",
+        },
+        secondary: {
+            light: '#A9A9A9',
+            main: '#D3D3D3',
+            dark: '#F5F5F5',
+        },
+        accent: {
+            main: "#FFA500",
+        }
+    }
+}
+
+export default theme;
