5bda8d9d63f283bb8c9df50aa6f57597bfa9f7d8
Teemu Tontti <teemu.tontti.tt@gmail.com>
2024-01-25 03:53:42 +0200

    ../connection.js: Add database connection functions.

---
+++ backend/database/connection.js
@@ -0,0 +1,65 @@
+require("dotenv").config();
+const mysql = require("mysql");
+
+const pool = mysql.createPool({
+    connectionLimit: 100,
+    host: process.env.HOST,
+    user: process.env.USER,
+    password: process.env.PASSWORD,
+    database: process.env.DATABASE,
+});
+
+export default class ConnectionFunctions {
+    findAll() {
+        return new Promise((resolve, reject) => {
+            const sql = "SELECT * FROM foods";
+            pool.query(sql, (err, results) => {
+                if (err) {
+                    reject(err);
+                    return;
+                } else {
+                    if (results.length === 0) {
+                        reject({
+                            code: 204,
+                            message: 'No foods in the database',
+                        });
+                        return;
+                    }
+                    resolve(results);
+                }
+            })
+        })
+    }
+    update(id, food) {
+        return new Promise((resolve, reject) => {
+            const sql = "UPDATE foods SET name = ?, calories = ?,"
+                        + "carbohydrates = ?, fats = ?, proteins = ?";
+            if (!food.name
+                || !food.calories
+                || !food.carbohydrates
+                || !food.fats
+                || !food.proteins
+            ) {
+                reject({code: 400, message: "Values can't be empty"});
+                return;
+            }
+
+            pool.query(sql, food, (err, res) => {
+                if (err) {
+                    reject(err);
+                    return;
+                }
+                else if (res.affectedRows === 0) {
+                    reject({
+                        code: 404,
+                        message: `There's no data with the id of ${id} in the`
+                                + `database`,
+                    })
+                    return;
+                }
+                resolve(res);
+                return;
+            })
+        })
+    }
+}
