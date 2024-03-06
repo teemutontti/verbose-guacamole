33947bd229d5d1d47b0d21c88cb851bf8fbc6047
Teemu Tontti <teemu.tontti.tt@gmail.com>
2024-03-06 20:48:30 +0200

    ../colorTheme.js: Edit color theme.

--- frontend/src/colorTheme.js
+++ frontend/src/colorTheme.js
@@ -1,18 +1,18 @@
 const theme = {
     dark: {
         primary: {
-            light: "#004080",
-            main: "#003366",
-            dark: "#001F3F",
+            light: "#0a2f52",
+            main: "#051b30",
+            dark: "#020e1a",
         },
         secondary: {
-            light: '#333333',
-            main: '#1A1A1A',
-            dark: '#000000',
+            light: "#333333",
+            main: "#FF3D5F",
+            dark: "#000000",
         },
         accent: {
             main: "#FFA500",
-        }
+        },
     },
     light: {
         primary: {
@@ -21,14 +21,14 @@ const theme = {
             dark: "#ADD8E6",
         },
         secondary: {
-            light: '#A9A9A9',
-            main: '#D3D3D3',
-            dark: '#F5F5F5',
+            light: "#A9A9A9",
+            main: "#D3D3D3",
+            dark: "#F5F5F5",
         },
         accent: {
             main: "#FFA500",
-        }
-    }
-}
+        },
+    },
+};
 
 export default theme;
