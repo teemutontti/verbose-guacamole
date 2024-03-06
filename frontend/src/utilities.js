a24829367812bdecb1014093b2a5f8325b2160de
Teemu Tontti <teemu.tontti.tt@gmail.com>
2024-03-06 20:58:40 +0200

    ../utilities.js: Edit PEP to PEV.

--- frontend/src/utilities.js
+++ frontend/src/utilities.js
@@ -1,9 +1,9 @@
-const calculatePep = (calories, proteins) => {
+const calculatePev = (calories, proteins) => {
     const proteinsTimed = proteins * 4;
     const proteinsInCalories = proteinsTimed / calories;
     const proteinEfficiencyPercentage = proteinsInCalories * 100;
 
     return proteinEfficiencyPercentage.toFixed(1);
-}
+};
 
-export default calculatePep;
+export default calculatePev;
