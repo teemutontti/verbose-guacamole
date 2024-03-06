18312c88ecd0235125ae2b1da7f206028e76e9f4
Teemu Tontti <teemu.tontti.tt@gmail.com>
2024-03-06 20:23:15 +0200

    ../connection.js: Fix save to give parameters as an array.

--- backend/database/connection.js
+++ backend/database/connection.js
@@ -21,34 +21,29 @@ const connectionFunctions = {
                     if (results.length === 0) {
                         reject({
                             code: 204,
-                            message: 'No foods in the database',
+                            message: "No foods in the database",
                         });
                         return;
                     }
                     resolve(results);
                 }
-            })
-        })
+            });
+        });
     },
     save(food) {
         return new Promise((resolve, reject) => {
-            const sql = "INSERT INTO foods (name, calories, carbohydrates, "
-                + "fats, proteins) VALUES (?, ?, ?, ?, ?)";
+            const sql = "INSERT INTO foods (name, calories, carbohydrates, fats, proteins, barcode) VALUES (?, ?, ?, ?, ?)";
 
-            if (!food.name
-                || !food.calories
-                || !food.carbohydrates
-                || !food.fats
-                || !food.proteins
-            ) {
-                reject({code: 400, message: "Values can't be empty"});
+            if (!food.name || !food.calories || !food.carbohydrates || !food.fats || !food.proteins) {
+                reject({ code: 400, message: "Values can't be empty" });
                 return;
             }
 
-            pool.query(sql, food, (err, res) => {
-                err ? reject(err) : resolve(res);
-            })
-        })
+            const foodList = [food.name, food.calories, food.carbohydrates, food.fats, food.proteins, food.barcode];
+            pool.query(sql, foodList, (err, res) => {
+                err ? reject({ code: 500, message: err }) : resolve(res);
+            });
+        });
     },
     update(id, food) {
         return new Promise((resolve, reject) => {
